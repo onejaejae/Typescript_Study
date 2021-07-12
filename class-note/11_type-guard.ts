ba3cefb5bb628b0fac5899e923468c7341b882c8
Cho Won Jae <yoteamo7@naver.com>
2021-07-12 13:23:07 +0900

    타입 호환 학습

--- class-note/11_type-guard.ts
+++ class-note/11_type-guard.ts
@@ -1,25 +1,25 @@
-interface Developer {
-    name : string;
-    skill: string
-}
+// interface Developer {
+//     name : string;
+//     skill: string
+// }
 
-interface Person {
-    name: string;
-    age: number;
-}
+// interface Person {
+//     name: string;
+//     age: number;
+// }
 
-function introduce(): Developer | Person {
-    return { name : "wonjae", skill : "ts", age:20}
-}
-var tony = introduce()
-console.log(tony.name)
+// function introduce(): Developer | Person {
+//     return { name : "wonjae", skill : "ts", age:20}
+// }
+// var tony = introduce()
+// console.log(tony.name)
 
-function isDeveloper(target: Developer | Person): target is Developer{
-    return (target as Developer).skill !== undefined;
-}
+// function isDeveloper(target: Developer | Person): target is Developer{
+//     return (target as Developer).skill !== undefined;
+// }
 
-if(isDeveloper(tony)){
-    console.log(tony.skill)
-}else{
-    console.log(tony.name)
-}
+// if(isDeveloper(tony)){
+//     console.log(tony.skill)
+// }else{
+//     console.log(tony.name)
+// }
---
+++ class-note/12_type-compatibility.ts
@@ -0,0 +1,45 @@
+//인터페이스
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+}
+
+var developer: Developer;
+var person: Person;
+
+//developer = person
+person= developer
+
+// 함수
+var add = function(a:number){
+    // ...
+}
+
+var sum = function(a:number, b:number){
+    // ...
+}
+
+sum = add
+add = sum
+
+// 제네릭
+interface Empty<T> {
+    // ..
+}
+
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+
+empty1 = empty2
+empty2 = empty1
+
+interface notEmpty<T> {
+    data: T;
+}
+
+var notEmpty1:notEmpty<number>;
+var notEmpty2:notEmpty<string>;
